716890d718c1d657d5a24f182c7988aae0efa83b
ricardojunior02 <ricardojunior199502@gmail.com>
2021-06-03 15:56:08 -0300

    Model de users

---
+++ src/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
+
+const User = mongoose.model('User', schema);
+
+module.exports = User;
